c8fd7af35040ce2ab4b85cf02413d44e3afba35c
Alex Waite <alexanderconorwaite@gmail.com>
2020-05-11 00:24:49 +0930

    Server returns both pages

--- server.js
+++ server.js
@@ -18,15 +18,14 @@ app.listen(PORT, function (error) {
     if (error) {
         console.log("You made a mistake")
     } else {
-        console.log(`listening on port: https://locahost:${PORT}`)
+        console.log(`listening on port: locahost:${PORT}`)
     }
 })
 
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "./index.html"))
+})
 
 app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "../public/notes.html"));
-});
-
-app.post("/notes", req, async function (res) {
-
+    res.sendFile(path.join(__dirname, "./notes.html"))
 })
